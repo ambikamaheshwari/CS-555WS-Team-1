42abb7fc86f4213f6b669dac56ee916d478ef313
Ambika Maheshwari <ambika.maheshwari17@gmail.com>
2015-03-29 20:36:08 -0400

    Update maingedcom.java

--- CS555WS/src/cs555ws/project/maingedcom.java
+++ CS555WS/src/cs555ws/project/maingedcom.java
@@ -9,25 +9,27 @@ public class maingedcom {
 		 
 		Datastructure ds = new Datastructure();
 		ds.readFile("final1.ged");
-		//ds.indicount("final.ged");
-		//ds.famcount("final.ged");
-		//ds.birthdatecount("final.ged");
-		//ds.deathcount("final.ged");
-		//ds.birthcount("final.ged");
-		//ds.birthBeforeDeath("final.ged");
-		//ds.DivorceBeforeMarriage("final.ged");
-		//ds.DeathBeforeMarriage("final.ged");
-		//ds.MarriageBeforeBirth("final.ged");
-		//ds.divorceAfterDeath("final.ged");
-		//ds.divorceBeforeBirth("final.ged");
-		//ds.MarriageofSamePerson("final.ged");
-		ds.weddingcount("final1.ged");
-		ds.divorcecount("final1.ged");
-		ds.CoupleSameDday("final1.ged");
-		ds.IndividualSameDday("final1.ged");
-		ds.birthdateleap("final1.ged");
-		ds.deathdateleap("final1.ged");
-		ds.CoupleSameBday("final1.ged");
-		ds.IndividualSameBday("final1.ged");
+		ds.indicount("final.ged");
+		ds.famcount("final.ged");
+		ds.birthdatecount("final.ged");
+		ds.deathcount("final.ged");
+		ds.birthcount("final.ged");
+		ds.birthBeforeDeath("final.ged");
+		ds.DivorceBeforeMarriage("final.ged");
+		ds.DeathBeforeMarriage("final.ged");
+		ds.MarriageBeforeBirth("final.ged");
+		ds.divorceAfterDeath("final.ged");
+		ds.divorceBeforeBirth("final.ged");
+		ds.MarriageofSamePerson("final.ged");
+		ds.CoupleSameBday("final1.ged"); //AM
+		ds.CoupleSameDday("final1.ged"); //AM
+		ds.IndividualSameBday("final1.ged"); //HB
+		ds.weddingcount("final1.ged"); //PT
+		ds.divorcecount("final1.ged");//PT
+		ds.IndividualSameDday("final1.ged");//HB
+		ds.birthdateleap("final1.ged");//SA
+		ds.deathdateleap("final1.ged");//SA
+		
+	
 	}
 }

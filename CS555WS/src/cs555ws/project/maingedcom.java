b276469bdc62446030f8009b777707f5b2facf48
puneetlampard <puneettrivedi9@gmail.com>
2015-04-11 21:06:09 -0400

    Update maingedcom.java

--- CS555WS/src/cs555ws/project/maingedcom.java
+++ CS555WS/src/cs555ws/project/maingedcom.java
@@ -8,7 +8,7 @@ public class maingedcom {
 		// TODO Auto-generated method stub
 		 
 		Datastructure ds = new Datastructure();
-		ds.readFile("final1.ged");
+		ds.readFile("Project08.ged");
 		ds.indicount("final.ged");
 		ds.famcount("final.ged");
 		ds.birthdatecount("final.ged");
@@ -21,19 +21,21 @@ public class maingedcom {
 		ds.divorceAfterDeath("final.ged");
 		ds.divorceBeforeBirth("final.ged");
 		ds.MarriageofSamePerson("final.ged");
-		ds.CoupleSameBday("final1.ged"); //AM
-		ds.CoupleSameDday("final1.ged"); //AM
-		ds.IndividualSameBday("final1.ged"); //HB
-		ds.weddingcount("final1.ged"); //PT
-		ds.divorcecount("final1.ged");//PT
-		ds.IndividualSameDday("final1.ged");//HB
-		ds.birthdateleap("final1.ged");//SA
-		ds.deathdateleap("final1.ged");//SA
-		ds.printAgeChild("final1.ged");//AM
-		ds.printAgeOld("final1.ged");//AM
-		ds.widowcount("final1.ged");//PT
-		ds.widowercount("final1.ged");//PT
-		ds.marriagedateleap("final1.ged");//SA
-		ds.divorcedateleap("final1.ged");//SA
+		ds.CoupleSameBday("final1.ged");
+		ds.CoupleSameDday("final1.ged");
+		ds.IndividualSameBday("final1.ged");
+		ds.weddingcount("final1.ged");
+		ds.divorcecount("final1.ged");
+		ds.IndividualSameDday("final1.ged");
+		ds.birthdateleap("final1.ged");
+		ds.deathdateleap("final1.ged");
+		ds.printAgeChild("Project08.ged");//AM
+		ds.printAgeOld("Project08.ged");//AM
+		ds.widowcount("Project08.ged");//PT
+		ds.widowercount("Project08.ged");//PT
+		ds.printAgeTeen("Project08.ged");//HB
+		ds.printAgeAdult("Project08.ged");//HB
+		ds.marriagedateleap("Project08.ged");//SA
+		ds.divorcedateleap("Project08.ged");//SA
 	}
 }
